cc87716e63ca752f53619313e4c39e2d8c577caa
masb0ymas <n.fajri@outlook.com>
2024-10-14 11:25:38 +0700

    feat: add profile & check auth

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,14 +1,31 @@
 'use client'
 
+import _ from 'lodash'
+import { useRouter } from 'next/navigation'
 import { PropsWithChildren } from 'react'
+import useProfile from '~/data/query/useProfile'
+import VerifyPage from '../loader/VerifyPage'
+import Footer from './footer'
 import Header from './header'
 import classes from './partials/Layout.module.css'
 import Sidebar from './sidebar'
-import Footer from './footer'
 
 type IProps = PropsWithChildren
 
 export default function Layout({ children }: IProps) {
+  const router = useRouter()
+  const { data, isLoading, isFetching } = useProfile()
+
+  const fetchingData = isLoading || isFetching
+
+  if (fetchingData) {
+    return <VerifyPage loading={fetchingData} />
+  }
+
+  if (_.isEmpty(data) || _.isNil(data.email)) {
+    return router.push('/')
+  }
+
   return (
     <>
       <Header />
--- src/ui/auth/Login.tsx
+++ src/ui/auth/Login.tsx
@@ -8,18 +8,82 @@ import {
   Group,
   Paper,
   PasswordInput,
+  Stack,
   Text,
   TextInput,
   Title,
 } from '@mantine/core'
+import { useForm, zodResolver } from '@mantine/form'
+import { showNotification } from '@mantine/notifications'
+import { IconAlertCircle, IconCheck } from '@tabler/icons-react'
+import { useMutation } from '@tanstack/react-query'
+import _ from 'lodash'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import VerifyPage from '~/components/loader/VerifyPage'
+import { useStore } from '~/config/zustand'
+import { LoginEntity } from '~/data/entity/auth'
+import useProfile from '~/data/query/useProfile'
+import AuthRepository from '~/data/repository/auth'
+import authSchema from '~/data/schema/auth'
 
 export default function Login() {
   const router = useRouter()
 
-  function handleLogin() {
-    console.log('/dashboard')
-    router.push('/dashboard')
+  const { data, isLoading, isFetching } = useProfile()
+  const setAuthSession = useStore((state) => state.setAuthSession)
+
+  const form = useForm({
+    initialValues: { email: '', password: '', latitude: '', longitude: '' },
+    validate: zodResolver(authSchema.login),
+  })
+
+  const postLogin = useMutation({
+    mutationFn: (data: LoginEntity) => AuthRepository.signIn(data),
+    onSuccess: (data) => {
+      showNotification({
+        title: 'Success',
+        message: data.message,
+        color: 'green',
+        icon: <IconCheck size={18} stroke={1.5} />,
+      })
+
+      const state = { email: form.values.email, access_token: data.data.access_token }
+      setAuthSession(state)
+
+      router.push('/dashboard')
+    },
+  })
+
+  async function onFormSubmit() {
+    try {
+      await postLogin.mutateAsync(form.values)
+    } catch (error: any) {
+      console.log(error)
+
+      let message = 'Please check your email and password.'
+      if (error.response?.data?.message) {
+        message = error.response.data.message
+      }
+
+      showNotification({
+        title: 'Catch Error',
+        message: message,
+        color: 'red',
+        icon: <IconAlertCircle size={18} stroke={1.5} />,
+      })
+    }
+  }
+
+  const fetchingData = isLoading || isFetching
+
+  if (fetchingData) {
+    return <VerifyPage loading={fetchingData} />
+  }
+
+  // @ts-expect-error
+  if (!_.isEmpty(data) || !_.isNil(data?.email)) {
+    return router.push('/dashboard')
   }
 
   return (
@@ -29,19 +93,38 @@ export default function Login() {
         To become a great wizard, login with your account first.
       </Text>
 
-      <Paper shadow="lg" p={30} mt={30} radius="lg">
-        <TextInput radius="md" label="Email" placeholder="you@mantine.dev" required />
-        <PasswordInput radius="md" label="Password" placeholder="Your password" required mt="md" />
-        <Group justify="space-between" mt="lg">
-          <Checkbox label="Remember me" />
-          <Anchor component="button" size="sm">
-            Forgot password?
-          </Anchor>
-        </Group>
-        <Button fullWidth mt="xl" radius="md" onClick={() => handleLogin()}>
-          Sign in
-        </Button>
-      </Paper>
+      <form onSubmit={form.onSubmit(onFormSubmit)}>
+        <Paper shadow="lg" p={30} mt={30} radius="lg">
+          <Stack gap={10}>
+            <TextInput
+              radius="md"
+              label="Email"
+              placeholder="you@mantine.dev"
+              required
+              {...form.getInputProps('email')}
+            />
+
+            <PasswordInput
+              radius="md"
+              label="Password"
+              placeholder="Your password"
+              required
+              {...form.getInputProps('password')}
+            />
+          </Stack>
+
+          <Group justify="space-between" mt="lg">
+            <Checkbox label="Remember me" />
+            <Anchor component={Link} href={'/forgot-password'} size="sm">
+              Forgot password?
+            </Anchor>
+          </Group>
+
+          <Button fullWidth mt="xl" radius="md" type="submit">
+            Sign in
+          </Button>
+        </Paper>
+      </form>
     </Container>
   )
 }
