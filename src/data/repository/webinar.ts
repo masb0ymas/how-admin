c4e92f784d029d39bd883a43f0e996f128f93142
masb0ymas <n.fajri@outlook.com>
2024-10-15 11:25:55 +0700

    fix: update webinar repo

--- src/data/repository/webinar.ts
+++ src/data/repository/webinar.ts
@@ -6,6 +6,7 @@ export default class WebinarRepository {
   /**
    *
    * @param formData
+   * @param options
    * @returns
    */
   public static async create(formData: WebinarEntity, options?: AxiosRequestConfig) {
@@ -16,12 +17,38 @@ export default class WebinarRepository {
 
   /**
    *
+   * @param id
    * @param formData
+   * @param options
    * @returns
    */
   public static async update(id: string, formData: WebinarEntity, options?: AxiosRequestConfig) {
     const url = `${env.API_URL}/v1/webinar/${id}`
     const response = await axios.put(url, formData, options)
+    return response.data
+  }
+
+  /**
+   *
+   * @param id
+   * @param options
+   * @returns
+   */
+  public static async softDelete(id: string, options?: AxiosRequestConfig) {
+    const url = `${env.API_URL}/v1/webinar/soft-delete/${id}`
+    const response = await axios.delete(url, options)
+    return response.data
+  }
+
+  /**
+   *
+   * @param id
+   * @param options
+   * @returns
+   */
+  public static async forceDelete(id: string, options?: AxiosRequestConfig) {
+    const url = `${env.API_URL}/v1/webinar/force-delete/${id}`
+    const response = await axios.delete(url, options)
     return response.data
   }
 }
