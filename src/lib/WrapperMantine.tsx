6f5a5b55beb941d60e30e963b5196e6cbf7c57bd
masb0ymas <n.fajri@outlook.com>
2024-10-16 20:52:38 +0700

    feat: add router transition component

--- src/lib/WrapperMantine.tsx
+++ src/lib/WrapperMantine.tsx
@@ -3,8 +3,8 @@
 import { localStorageColorSchemeManager, MantineProvider } from '@mantine/core'
 import { ModalsProvider } from '@mantine/modals'
 import { Notifications } from '@mantine/notifications'
-import { NavigationProgress } from '@mantine/nprogress'
 import { PropsWithChildren } from 'react'
+import RouterTransition from '~/components/router-transition'
 import { env } from '~/config/env'
 import { theme } from '~/styles/theme'
 
@@ -17,12 +17,10 @@ export default function WrapperMantine({ children }: IProps) {
 
   return (
     <MantineProvider theme={theme} colorSchemeManager={colorSchemeManager}>
-      <ModalsProvider>
-        <NavigationProgress />
-        <Notifications />
+      <RouterTransition />
+      <Notifications />
 
-        {children}
-      </ModalsProvider>
+      <ModalsProvider>{children}</ModalsProvider>
     </MantineProvider>
   )
 }
