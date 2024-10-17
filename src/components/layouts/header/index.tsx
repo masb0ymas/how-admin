0fae2c1e1b4ba8f3fa2a1dd3a3b8cc2794b86a86
masb0ymas <n.fajri@outlook.com>
2024-10-17 17:31:43 +0700

    fix: update header for mobile version

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -1,20 +1,29 @@
 'use client'
 
-import { Group, rem, TextInput } from '@mantine/core'
+import { Group, rem, TextInput, ThemeIcon } from '@mantine/core'
 import { IconSearch } from '@tabler/icons-react'
 import classes from './partials/Header.module.css'
 import HeaderProfile from './partials/HeaderProfile'
+import { useViewportSize } from '@mantine/hooks'
 
 export default function Header() {
+  const { width } = useViewportSize()
+
   return (
     <header className={classes.header}>
       <Group justify="space-between">
-        <TextInput
-          leftSectionPointerEvents="none"
-          leftSection={<IconSearch style={{ width: rem(16), height: rem(16) }} />}
-          placeholder="Search"
-          radius="md"
-        />
+        {width > 480 ? (
+          <TextInput
+            leftSectionPointerEvents="none"
+            leftSection={<IconSearch style={{ width: rem(16), height: rem(16) }} />}
+            placeholder="Search"
+            radius="md"
+          />
+        ) : (
+          <ThemeIcon size="lg" radius="lg" variant="light">
+            <IconSearch size={22} />
+          </ThemeIcon>
+        )}
 
         <HeaderProfile />
       </Group>
