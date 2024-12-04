da37b6724ec01311d6cff6b9377ced62ff84684c
masb0ymas <n.fajri@outlook.com>
2024-12-04 22:07:23 +0700

    fix: update tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
+        serif: ['Faculty Glyphic', 'sans-serif'],
         sans: ['Open Sans', 'sans-serif'],
       },
       borderRadius: {
@@ -70,6 +71,30 @@ module.exports = {
           'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
           border: 'hsl(var(--sidebar-border))',
           ring: 'hsl(var(--sidebar-ring))',
+        },
+        'color-1': 'hsl(var(--color-1))',
+        'color-2': 'hsl(var(--color-2))',
+        'color-3': 'hsl(var(--color-3))',
+        'color-4': 'hsl(var(--color-4))',
+        'color-5': 'hsl(var(--color-5))',
+      },
+      animation: {
+        'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
+        rainbow: 'rainbow var(--speed, 2s) infinite linear',
+      },
+      keyframes: {
+        'border-beam': {
+          '100%': {
+            'offset-distance': '100%',
+          },
+        },
+        rainbow: {
+          '0%': {
+            'background-position': '0%',
+          },
+          '100%': {
+            'background-position': '200%',
+          },
         },
       },
     },
