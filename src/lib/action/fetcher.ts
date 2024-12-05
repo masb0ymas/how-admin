04d4f22c1b44a49f74f711cffb016a381ae8d3d1
masb0ymas <n.fajri@outlook.com>
2024-12-05 21:56:05 +0700

    feat: add lib action fetcher

---
+++ src/lib/action/fetcher.ts
@@ -0,0 +1,40 @@
+'use server'
+
+import axios, { AxiosInstance } from 'axios'
+import { auth } from '../auth'
+
+function createAxios({ baseURL }: { baseURL: string }) {
+  const instanceAxios = axios.create({ baseURL })
+
+  instanceAxios.interceptors.request.use(async (config) => {
+    const session = await auth()
+
+    const currentConfig = { ...config }
+    currentConfig.headers.Authorization = `Bearer ${session?.idToken}`
+
+    return currentConfig
+  })
+
+  instanceAxios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      return Promise.reject(error)
+    }
+  )
+
+  return instanceAxios
+}
+
+export default async function createFetchApi(baseUrl: string) {
+  let axiosInstance: AxiosInstance | null = null
+
+  return {
+    get default() {
+      if (!axiosInstance) {
+        axiosInstance = createAxios({ baseURL: baseUrl })
+      }
+
+      return axiosInstance
+    },
+  }
+}
