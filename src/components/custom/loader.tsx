03586281d098a1339df2896998e3b33f982a9b0c
masb0ymas <n.fajri@outlook.com>
2024-12-08 22:02:30 +0700

    fix: update loader component

--- src/components/custom/loader.tsx
+++ src/components/custom/loader.tsx
@@ -1,10 +1,14 @@
 import { IconLoader } from '@tabler/icons-react'
 
-export default function Loader() {
+type LoaderProps = {
+  label?: string
+}
+
+export default function Loader({ label = 'Loading...' }: LoaderProps) {
   return (
-    <div className="flex items-center justify-center gap-2 mt-10">
+    <div className="flex items-center justify-center gap-2 my-10">
       <IconLoader className="h-6 w-6 animate-spin" />
-      <span>Loading...</span>
+      <span>{label}</span>
     </div>
   )
 }
