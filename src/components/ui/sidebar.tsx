73413263cd3db1266ebb280c6a18c8a5312afe92
masb0ymas <n.fajri@outlook.com>
2024-12-19 21:19:33 +0700

    fix: update sidebar effect

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -314,7 +314,7 @@ const SidebarInset = React.forwardRef<HTMLDivElement, React.ComponentProps<'main
         className={cn(
           'relative flex min-h-svh flex-1 flex-col bg-background transition-all ease-in-out duration-500 delay-500',
           'peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow',
-          state === 'expanded' ? 'max-w-[calc(100%-16rem)]' : 'max-w-[calc(100%-3rem)]',
+          state === 'expanded' ? 'max-w-[calc(100%-16rem)]' : 'max-w-[calc(100%-1rem)]',
           className
         )}
         {...props}
