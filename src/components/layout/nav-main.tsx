bfddeffb6ee53f22e9766680d67edb4b6f5cd5da
masb0ymas <n.fajri@outlook.com>
2024-12-19 16:33:22 +0700

    fix: add sidebar active menu

--- src/components/layout/nav-main.tsx
+++ src/components/layout/nav-main.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import {
   SidebarGroup,
   SidebarGroupLabel,
@@ -10,26 +11,32 @@ import {
 import { sidebarMenu } from './sidebar-menu'
 
 export function NavMain() {
+  const pathname = usePathname()
+
   return (
     <SidebarGroup>
       {sidebarMenu.map((menu) => (
         <div className="mb-2" key={menu.title}>
           <SidebarGroupLabel>{menu.title}</SidebarGroupLabel>
-          {menu.items.map((item) => (
-            <SidebarMenu key={item.title}>
-              <SidebarMenuButton
-                tooltip={item.title}
-                key={item.title}
-                // isActive={item.isActive}
-                asChild
-              >
-                <Link href={item.url}>
-                  {item.icon && <item.icon />}
-                  <span>{item.title}</span>
-                </Link>
-              </SidebarMenuButton>
-            </SidebarMenu>
-          ))}
+          {menu.items.map((item) => {
+            const isActive = item.url === '/' ? pathname === '/' : pathname.startsWith(item.url)
+
+            return (
+              <SidebarMenu key={item.title}>
+                <SidebarMenuButton
+                  tooltip={item.title}
+                  key={item.title}
+                  isActive={isActive}
+                  asChild
+                >
+                  <Link href={item.url}>
+                    {item.icon && <item.icon />}
+                    <span>{item.title}</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenu>
+            )
+          })}
         </div>
       ))}
     </SidebarGroup>
